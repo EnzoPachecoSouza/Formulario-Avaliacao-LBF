8a338d7cd895ac8471da790e7e1d4d5f7b4bc63a
Enzo Pacheco <enzo.pach@gmail.com>
2025-03-21 18:40:25 -0300

    mudando o @ do login

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ function verificarLogin(event) {
     event.preventDefault();
 
     const usuarios = [
-        { nome: "Jurado Palanque 1", email: "juradopalanque1@gmail.com", senha: "1234" },
-        { nome: "Jurado Palanque 2", email: "juradopalanque2@gmail.com", senha: "1234" },
-        { nome: "Jurado Palanque 3", email: "juradopalanque3@gmail.com", senha: "1234" },
-        { nome: "Jurado Palanque 4", email: "juradopalanque4@gmail.com", senha: "1234" },
-        { nome: "Jurado Palanque 5", email: "juradopalanque5@gmail.com", senha: "1234" },
-        { nome: "Jurado Palanque 6", email: "juradopalanque6@gmail.com", senha: "1234" },
-        { nome: "Jurado Uniformidade/Instrumental", email: "juradouniformidadeinstrumental@gmail.com", senha: "1234" },
-        { nome: "Jurado Check-In", email: "juradocheckin@gmail.com", senha: "1234" },
-        { nome: "Jurado Cronômetro", email: "juradocronometro@gmail.com", senha: "1234" },
-        { nome: "Jurado Check List", email: "juradochecklist@gmail.com", senha: "1234" },
-        { nome: "Dados de Apresentação", email: "dados@gmail.com", senha: "1234" },
-        { nome: "Jurado Faixa Etária", email: "juradofaixaetaria@gmail.com", senha: "1234" }
+        { nome: "Jurado Palanque 1", email: "juradopalanque1@applbf.org", senha: "1234" },
+        { nome: "Jurado Palanque 2", email: "juradopalanque2@applbf.org", senha: "1234" },
+        { nome: "Jurado Palanque 3", email: "juradopalanque3@applbf.org", senha: "1234" },
+        { nome: "Jurado Palanque 4", email: "juradopalanque4@applbf.org", senha: "1234" },
+        { nome: "Jurado Palanque 5", email: "juradopalanque5@applbf.org", senha: "1234" },
+        { nome: "Jurado Palanque 6", email: "juradopalanque6@applbf.org", senha: "1234" },
+        { nome: "Jurado Uniformidade/Instrumental", email: "juradouniformidadeinstrumental@applbf.org", senha: "1234" },
+        { nome: "Jurado Check-In", email: "juradocheckin@applbf.org", senha: "1234" },
+        { nome: "Jurado Cronômetro", email: "juradocronometro@applbf.org", senha: "1234" },
+        { nome: "Jurado Check List", email: "juradochecklist@applbf.org", senha: "1234" },
+        { nome: "Dados de Apresentação", email: "dados@applbf.org", senha: "1234" },
+        { nome: "Jurado Faixa Etária", email: "juradofaixaetaria@applbf.org", senha: "1234" }
     ];
 
     const email = document.getElementById("email").value;
