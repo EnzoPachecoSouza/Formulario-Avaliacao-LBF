835eeb72e572b3d931ed238515531ce9ea3df95e
Enzo Pacheco <enzo.pach@gmail.com>
2025-04-06 18:14:58 -0300

    excluindo eteas

--- form.js
+++ form.js
@@ -411,7 +411,7 @@ const bandasDisponiveis = [
     // "11 • Fundação Lia Maria Aguiar- Campos do Jordão/SP",
     "12 • Fundação Lia Maria Aguiar- Campos do Jordão/SP",
     "13 • Fundação Lia Maria Aguiar- Campos do Jordão/SP",
-    "14 • ETEAAS - Campo dos Goytacazes/RJ",
+    // "14 • ETEAAS - Campo dos Goytacazes/RJ",
     "15 • Lyra Bragança - Bragança Paulista/SP",
     "16 • Marista PIO Xll - Ponta Grossa/PR",
     "17 • Fundação Lia Maria Aguiar- Campos do Jordão/SP",
